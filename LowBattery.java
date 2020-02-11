e2eb061ae043e968d48cb7fa077708e47f049f6c
Principor <c.jewszel@gmail.com>
2020-02-11 11:46:31 +0000

    Added low battery

---
+++ LowBattery.java
@@ -0,0 +1,32 @@
+import lejos.hardware.Battery;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
+
+public class LowBattery implements Behavior{
+
+	MovePilot pilot;
+	
+	LowBattery(MovePilot p){
+		pilot = p;
+	}
+	
+	public boolean takeControl() {
+
+		return Battery.getVoltage() < 1;
+	}
+
+	public void action() {
+		pilot.stop();
+		LCD.clear();
+		LCD.drawString("LOW BATTERY", 1, 1);
+		Sound.beep();
+		Delay.msDelay(1000);
+	}
+
+	public void suppress() {
+	}
+
+}
