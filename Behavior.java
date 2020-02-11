17af232e3379cafcc226945b0fc257c5dccddc86
Principor <c.jewszel@gmail.com>
2020-02-11 10:32:42 +0000

    initial commit

---
+++ Behavior.java
@@ -0,0 +1,29 @@
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
+
+public class Behavior {
+
+	final static float WHEEL_DIAMETER = 56;
+	final static float AXLE_LENGTH = 190;
+	final static float ANGULAR_SPEED = 100;
+	final static float LINEAR_SPEED = 70;
+	
+	public static void main(String[] args) {
+		BaseRegulatedMotor mLeft = new EV3LargeRegulatedMotor(MotorPort.A);
+		Wheel wLeft = WheeledChassis.modelWheel(mLeft, WHEEL_DIAMETER).offset(-AXLE_LENGTH / 2);
+		BaseRegulatedMotor mRight = new EV3LargeRegulatedMotor(MotorPort.B);
+		Wheel wRight = WheeledChassis.modelWheel(mRight, WHEEL_DIAMETER).offset(AXLE_LENGTH / 2);
+		
+		Chassis chassis = new WheeledChassis(new Wheel[] { wRight, wLeft}, WheeledChassis.TYPE_DIFFERENTIAL);
+		
+		MovePilot pilot = new MovePilot(chassis);
+		
+		
+	}
+	
+}
